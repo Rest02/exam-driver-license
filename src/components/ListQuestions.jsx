b9a3faaae6c60c8454968da3ddd909503d2b2c0d
Rest <code.rest.dev@gmail.com>
2024-01-09 03:51:39 -0300

    Version 1.4 con array listo en el estado para mostrar apartado de ver correción

--- src/components/ListQuestions.jsx
+++ src/components/ListQuestions.jsx
@@ -5,23 +5,42 @@ function ListQuestions() {
   const [preguntas, setPreguntas] = useState([]);
   const [puntos, setPuntos] = useState(0);
   const [preguntaRespondida, setPreguntaRespondida] = useState([]);
+  const [eleccion, setEleccion] = useState([]) 
 
   useEffect(() => {
     setPreguntas(examen);
   }, []);
 
-  function correcion(opcion, e) {
-    if (opcion.isCorrect == true) {
+  function correcion(a, e) {
+    if (a.isCorrect == true) {
       alert("Es correcta");
       setPuntos(puntos + 1);
       setPreguntas(preguntas.filter((s) => s.pregunta !== e.pregunta));
       setPreguntaRespondida([...preguntaRespondida, e])
+      setEleccion([...eleccion, ])
+      const real_response = e.opciones.find((e) => e.isCorrect == true)
+      almacenarRespuestaUsuario_RespueReal(a.id, e.pregunta, a.opcion, real_response)
+      console.log(real_response)
+      
+      console.log(eleccion)
     } else {
       alert("Es incorrecta");
       setPreguntas(preguntas.filter((s) => s.pregunta !== e.pregunta))
       setPreguntaRespondida([...preguntaRespondida, e])
+      const real_response = e.opciones.find((e) => e.isCorrect == true)
+      almacenarRespuestaUsuario_RespueReal(a.id, e.pregunta, a.opcion, real_response)
 
     }
+  }
+
+
+  const almacenarRespuestaUsuario_RespueReal = (elvalor, pregunta, respuesta, real_response) => {
+    setEleccion([...eleccion, {
+      pregunta: pregunta,
+      id: elvalor,
+      respuesta : respuesta,
+      respuestaReal : real_response.opcion
+    }])
   }
 
 
@@ -32,9 +51,8 @@ function ListQuestions() {
   const mostrarResultado = () => {
     if(preguntas.length == 0){
       return <div>Haz finalizado el cuestionaro , tu puntuacion es de {puntos} punto/s de 35
-      <button>Ver correción </button>
+      <button>Ver correción</button>
       </div>
-      
     }
   }
 
--- src/examen.js
+++ src/examen.js
@@ -1,29 +1,30 @@
 export const examen = [
     {
         pregunta: "Su vehículo se desvía hacia un lado cuando usted frena. Usted debería:",
+        respuesta: 4,
         opciones: [        
-            {opcion: "Cambiar los neumáticos de un lado hacia el otro y viceversa.", isCorrect: false},
-            {opcion: "Bombear el pedal al frenar." , isCorrect: false},
-            {opcion: "Usar su freno de mano." , isCorrect: false},
-            {opcion: "Consultar con su mecánico lo antes posible." , isCorrect: true}]
+            {id: 1, opcion: "Cambiar los neumáticos de un lado hacia el otro y viceversa.", isCorrect: false},
+            {id: 2,opcion: "Bombear el pedal al frenar." , isCorrect: false},
+            {id: 3,opcion: "Usar su freno de mano." , isCorrect: false},
+            {id: 4,opcion: "Consultar con su mecánico lo antes posible." , isCorrect: true}]
    
     },
-    {
-        pregunta: "Su vehículo se desvía hacia un lado cuando usted frena. ¿Cuál es la falla más probable?",
-        opciones: [        
-            {opcion: "Un bajo nivel del líquido de frenos.", isCorrect: false},
-            {opcion: "Su freno de mano está todavía puesto." , isCorrect: false},
-            {opcion: "Frenos mal ajustados." , isCorrect: true},
-            {opcion: "Neumáticos con presión de aire inadecuada." , isCorrect: false}]
+    // {
+    //     pregunta: "Su vehículo se desvía hacia un lado cuando usted frena. ¿Cuál es la falla más probable?",
+    //     opciones: [        
+    //         {opcion: "Un bajo nivel del líquido de frenos.", isCorrect: false},
+    //         {opcion: "Su freno de mano está todavía puesto." , isCorrect: false},
+    //         {opcion: "Frenos mal ajustados." , isCorrect: true},
+    //         {opcion: "Neumáticos con presión de aire inadecuada." , isCorrect: false}]
   
-    },
-    {
-        pregunta: "Mientras conduce usted siente un fuerte olor a gasolina. ¿Qué debería hacer usted?",
-        opciones: [        
-            {opcion: "No preocuparse, ya que sólo son los gases de escape.", isCorrect: false},
-            {opcion: "Continuar a una velocidad reducida. " , isCorrect: false},
-            {opcion: "Continuar porque sabe que se detendrá algunos kilómetros más allá." , isCorrect: false},
-            {opcion: "Detenerse e investigar el problema." , isCorrect: true}]
+    // },
+    // {
+    //     pregunta: "Mientras conduce usted siente un fuerte olor a gasolina. ¿Qué debería hacer usted?",
+    //     opciones: [        
+    //         {opcion: "No preocuparse, ya que sólo son los gases de escape.", isCorrect: false},
+    //         {opcion: "Continuar a una velocidad reducida. " , isCorrect: false},
+    //         {opcion: "Continuar porque sabe que se detendrá algunos kilómetros más allá." , isCorrect: false},
+    //         {opcion: "Detenerse e investigar el problema." , isCorrect: true}]
          
-    }
+    // }
 ]
