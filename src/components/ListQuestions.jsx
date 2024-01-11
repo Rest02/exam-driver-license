f4eea339de9e836528bdf5ff604612834d98c407
Rest <code.rest.dev@gmail.com>
2024-01-10 22:01:48 -0300

    Version 1.6 con listar preguntas aleatorias de forma al azar sin que se repitan

--- src/components/ListQuestions.jsx
+++ src/components/ListQuestions.jsx
@@ -1,67 +1,104 @@
 import { examen } from "../examen";
-import RandomQuestions from './RandomQuestions'
+import RandomQuestions from "./RandomQuestions";
 import React, { useEffect, useState } from "react";
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function ListQuestions() {
   const [preguntas, setPreguntas] = useState([]);
   const [puntos, setPuntos] = useState(0);
   const [preguntaRespondida, setPreguntaRespondida] = useState([]);
-  const [eleccion, setEleccion] = useState([]) 
+  const [eleccion, setEleccion] = useState([]);
 
   useEffect(() => {
-    setPreguntas(examen);
+    setPreguntas(obtenerPreguntasAleatorias(examen, 5));
   }, []);
+
+  const obtenerPreguntasAleatorias = (elExamen, cantidad) => {
+    const numberHistory = [];
+    const preguntasAleatorias = [];
+
+    for (let j = 0; j < cantidad; j++) {
+      const randomIndex = random(0, elExamen.length - 1);
+      const pregunta = elExamen[randomIndex];
+
+      if (!numberHistory.includes(randomIndex)) {
+        numberHistory.push(randomIndex);
+        preguntasAleatorias.push(pregunta);
+      } else {
+        j--;
+      }
+    }
+
+    return preguntasAleatorias;
+  };
 
   function correcion(a, e) {
     if (a.isCorrect == true) {
       alert("Es correcta");
       setPuntos(puntos + 1);
       setPreguntas(preguntas.filter((s) => s.pregunta !== e.pregunta));
-      setPreguntaRespondida([...preguntaRespondida, e])
-      setEleccion([...eleccion, ])
-      const real_response = e.opciones.find((e) => e.isCorrect == true)
-      almacenarRespuestaUsuario_RespueReal(a.id, e.pregunta, a.opcion, real_response)
-      console.log(real_response)
-      
-      console.log(eleccion)
+      setPreguntaRespondida([...preguntaRespondida, e]);
+      setEleccion([...eleccion]);
+      const real_response = e.opciones.find((e) => e.isCorrect == true);
+      almacenarRespuestaUsuario_RespueReal(
+        a.id,
+        e.pregunta,
+        a.opcion,
+        real_response
+      );
+      console.log(real_response);
+
+      console.log(eleccion);
     } else {
       alert("Es incorrecta");
-      setPreguntas(preguntas.filter((s) => s.pregunta !== e.pregunta))
-      setPreguntaRespondida([...preguntaRespondida, e])
-      const real_response = e.opciones.find((e) => e.isCorrect == true)
-      almacenarRespuestaUsuario_RespueReal(a.id, e.pregunta, a.opcion, real_response)
-
+      setPreguntas(preguntas.filter((s) => s.pregunta !== e.pregunta));
+      setPreguntaRespondida([...preguntaRespondida, e]);
+      const real_response = e.opciones.find((e) => e.isCorrect == true);
+      almacenarRespuestaUsuario_RespueReal(
+        a.id,
+        e.pregunta,
+        a.opcion,
+        real_response
+      );
     }
   }
 
-
-  const almacenarRespuestaUsuario_RespueReal = (elvalor, pregunta, respuesta, real_response) => {
-    setEleccion([...eleccion, {
-      pregunta: pregunta,
-      id: elvalor,
-      respuesta : respuesta,
-      respuestaReal : real_response.opcion
-    }])
-  }
-
+  const almacenarRespuestaUsuario_RespueReal = (
+    elvalor,
+    pregunta,
+    respuesta,
+    real_response
+  ) => {
+    setEleccion([
+      ...eleccion,
+      {
+        pregunta: pregunta,
+        id: elvalor,
+        respuesta: respuesta,
+        respuestaReal: real_response.opcion,
+      },
+    ]);
+  };
 
   const mostrarResultado = () => {
-    if(preguntas.length == 0){
-      return <div>Haz finalizado el cuestionaro , tu puntuacion es de {puntos} punto/s de 35
-      <button>Ver correción</button>
-      </div>
+    if (preguntas.length == 0) {
+      return (
+        <div>
+          Haz finalizado el cuestionaro , tu puntuacion es de {puntos} punto/s
+          de 35
+          <button>Ver correción</button>
+        </div>
+      );
     }
-  }
-
-
-  
-
-
+  };
 
   return (
     <div>
-      <RandomQuestions correcion = {correcion} preguntas = {preguntas}/>
-  
+      <RandomQuestions correcion={correcion} preguntas={preguntas} />
+
       {mostrarResultado()}
     </div>
   );
